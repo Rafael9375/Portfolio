5aae34efc74883485007338daec693c6289f5d2e
$pronet <rafael.anexo@gmail.com>
2019-01-20 12:12:40 -0200

    MVC criado

---
+++ Portfolio/Portfolio.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portfolio.MVC.Startup))]
+namespace Portfolio.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
